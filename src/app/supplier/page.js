baff0eda2e0ee0ae4d12a56834546fdd7f5defbd
rodrigueness7 <alex.correarodrigues@gmail.com>
2024-12-12 18:03:09 -0300

    Modifying components table and placing button on supplier screen.

--- src/app/supplier/page.js
+++ src/app/supplier/page.js
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import Table from "../../../components/table"
+import Link from "next/link"
 
 
 const fetchDataSupplier = async (token) => {
@@ -25,7 +26,10 @@ export default async function Supplier() {
     const supplier = await fetchDataSupplier(token)
 
     return (
-        <div className="bg-gray-100 py-8 overflow-x-auto h-screen">
+        <div className="bg-gray-100 py-8 overflow-x-auto h-screen px-8">
+            <div className="flex mb-8 lg:px-2 sm:px-12">
+                <Link href={'../supplier/registerSupplier'}><button className='p-2 bg-indigo-500 rounded-lg text-white'>Novo Fonercedor</button></Link>
+            </div>
             <div>
                 <Table Table={'min-w-full table-auto bg-white shadow-md rounded-lg overflow-hidden'} TrThead={'bg-gray-800 text-white'} Th={'py-2 px-4 text-left'} TrTbody={'border-b'} Td={'py-2 px-4'} headers={['Fornecedor', 'Email', 'Contato', 'CNPJ', 'Endereço', 'Cep', 'Estado', 'Cidade' ]} data={supplier} attributos={['supplier', 'email', 'contact', 'CNPJ', 'address', 'zipCode', 'state', 'city']} id={'idSupplier'}></Table>
             </div>
