f847792d832e0994d431bd0e7906844126ae36b0
rodrigueness7 <alex.correarodrigues@gmail.com>
2025-03-11 18:22:28 -0300

    modifying the registration profile permission screen

--- src/app/profile/registerProfile/registerProfile.js
+++ src/app/profile/registerProfile/registerProfile.js
@@ -2,53 +2,55 @@
 import { useState } from "react";
 import InputForm from "../../../../components/InputForm";
 import addData from "../../../../utils/addData";
-import InputCheckbox from "../../../../components/inputCheckBox";
+import fetchData from "../../../../utils/fetchData";
 
 
 export default function RegisterProfile({ token, dataPermission }) {
 
     const [profile, setProfile] = useState('')
-    const [isCheckBox, setIsCheckBox] = useState({
-        Gravar: false,
-        Ler: false,
-        Atualizar: false,
-        Deletar: false
-    })
+    const [selectedPermissions, setSelectPermissions] = useState(
+        dataPermission.map(permission => ({
+            id: permission.idPermission,
+            value: permission.permission + ' - ' + permission.section, 
+            checked: false}))
+    )
     const [result, setResult] = useState('')
 
     const changeProfile = (e) => {
         setProfile(e.target.value)
     }
 
-    const handleCheckbox = (e) => {
-        const { name, checked } = e.target
-        setIsCheckBox((prevState) => ({
-            ...prevState,
-            [name]: checked
-        }))
+    const handleChange = (e) => {
+        const { value, checked } = e.target
+        setSelectPermissions(prev => prev.map(
+            p => p.value === value ? {...p, checked} : p
+        ))
 
     }
-
-    const addProfile = async () => {
-        const selectedPermission = Object.keys(isCheckBox).filter(
-            (keys) => isCheckBox[keys] == true
-        )
-
-        const dataProfile = { profile: profile }
-        const dataProfile_permission = {
-            idprofile_permission: 0,
+    const addProfile = async (e) => {
+        e.preventDefault()
+        let dataProfile = {
             idProfile: 0,
-            idPermission: selectedPermission,
             profile: profile
         }
 
-        await addData('http://localhost:3001/addProfile', dataProfile, token, setResult)
-     
-        
+        addData('http://localhost:3001/addProfile', dataProfile, token, setResult)
+       
+      setTimeout(async () => {
+        let fetchProfile  = await fetchData('http://localhost:3001/findAllProfile', token)
+        let idProfile = fetchProfile.find(item => item.profile === profile).idProfile 
+    
+        let dataProfile_permission = selectedPermissions.map(itens => ({
+         profile: profile,   
+         idProfile_permission: 0,
+         idProfile: idProfile,
+         idPermission: itens.id,
+         allow: itens.checked
+        }))
+        addData('http://localhost:3001/addProfile_permission', dataProfile_permission, token, setResult)
+        console.log(dataProfile_permission)
+      },3000)
 
-        setInterval(async() =>{
-            await addData('http://localhost:3001/addProfile_permission', dataProfile_permission, token, setResult)
-        }, 2000)
     }
 
     return (
@@ -57,12 +59,14 @@ export default function RegisterProfile({ token, dataPermission }) {
                 <h1 className="text-2xl font-semibold text-center text-gray-800 mb-6">Adicionar Perfil </h1>
                 <form className="grid grid-cols-1 gap-x-8 gap-y-4">
                     <InputForm classNameLabe={"block text-sm font-medium text-gray-700"} classNameInput={"mt-2 block w-full px-4 py-2 bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"} div={'mb-4'} label={"Perfil"} name={"Profile"} type={'text'} value={profile} onchange={changeProfile}></InputForm>
-                    <div className="flex">
-                        <InputCheckbox nameLabel={'Gravar'} inputName={'Gravar'} checked={isCheckBox.Gravar} handleCheckbox={handleCheckbox} ></InputCheckbox>
-                        <InputCheckbox nameLabel={'Ler'} inputName={'Ler'} checked={isCheckBox.Ler} handleCheckbox={handleCheckbox} ></InputCheckbox>
-                        <InputCheckbox nameLabel={'Atualizar'} inputName={'Atualizar'} checked={isCheckBox.Atualizar} handleCheckbox={handleCheckbox} ></InputCheckbox>
-                        <InputCheckbox nameLabel={'Deletar'} inputName={'Deletar'} checked={isCheckBox.Deletar} handleCheckbox={handleCheckbox} ></InputCheckbox>
-                    </div>
+                    <div className="grid grid-cols-2 gap-4 max-h-60 overflow-y-auto">
+                          {selectedPermissions.map(({id ,value, checked}) => (
+                              <label className="flex items-center" key={id}>
+                                  <input type="checkbox" value={value} checked={checked} onChange={handleChange}/>
+                                 <span className="ml-2"> {value}</span>
+                              </label>
+                          ))}
+                      </div>
                 </form>
                 <div className="mb-6">
                     <button onClick={addProfile} className="w-full mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 roundedw-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 ">Adicionar</button>
