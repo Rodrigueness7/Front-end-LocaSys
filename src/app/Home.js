bd6043fbd2db817eeff298e2712c7796d8276366
rodrigueness7 <alex.correarodrigues@gmail.com>
2025-05-14 14:51:30 -0300

    was create input for insert file.

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,23 +1,50 @@
 'use client'
 
-import Modal from "@/components/modal";
+
+import addData from "@/utils/addData";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"
+import { useState } from "react";
 
 
-export default function PageHome() {
+export default function PageHome({token}) {
     const router = useRouter()
-    
+    const [show, setShow] = useState(false)
+    const [file, setFile] = useState(null)
+    const [result, setResult] = useState(null)
+
 
     const handleExit = () => {
         const clearCookie = (name) => {
             document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-          };
-          
-          clearCookie('token');
-          localStorage.clear()
-          router.push('./login')
+        };
+
+        clearCookie('token');
+        localStorage.clear()
+        router.push('./login')
     }
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        setFile(file);
+        
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();    
+        formData.append('file', file);
+
+        
+         await fetch('http://localhost:3001/uploadFile', {
+            method: 'POST',
+            headers: {
+                'Authorization': token
+            },
+            body: formData
+        })
+
+    }   
 
     return (
         <div className="flex">
@@ -42,12 +69,28 @@ export default function PageHome() {
                     <Link href={'./profile'} className="hover:text-blue-500 transition duration-300">Perfil</Link>
                 </div>
                 <div>
-                    <Link href={'./logs'} className="hover:text-blue-500 transition duration-300">Logs</Link>
+                    <Link href={'./logs'} className="hover:text-blue-500 transition duration-300">Log</Link>
+                </div>
+                <div>
+                    <button onClick={() => setShow(true)} className="hover:text-blue-500 transition duration-300">Importa</button>
                 </div>
                 <div>
                     <button onClick={handleExit}>Sair</button>
                 </div>
             </div>
+            {show && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="bg-white p-6 rounded shadow-lg">
+                        <h2 className="text-lg font-bold mb-4">Importar</h2>
+                        <p>Selecione o arquivo para importar.</p>
+                        <form onSubmit={handleSubmit} encType="multipart/form-data">
+                             <input type="file" accept=".xlsx" onChange={handleFileUpload}/>
+                            <button type="submit" className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Enviar</button>
+                        </form>
+                        <button onClick={() => setShow(false)} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Fechar</button> 
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,6 @@ export default async function Home() {
     }
 
     return (
-        <PageHome></PageHome>
+        <PageHome token={token}></PageHome>
     )
 }
