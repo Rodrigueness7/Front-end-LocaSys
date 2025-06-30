ffe20a5b17badca56dc7c3b9b4f699c3343691ab
arodrigues <alex.correarodrigues@gmail.com>
2025-06-30 10:46:35 -0300

    modifing pagination.

--- src/components/table.js
+++ src/components/table.js
@@ -20,6 +20,26 @@ export default function Table({ Table, TrThead, Th, TrTbody, Td, headers, data, 
         }
     }
 
+     const generatePageList = () => {
+        const pages = [];
+        
+        if (totalPages <= 5) {
+            for (let i = 1; i <= totalPages; i++) {
+                pages.push(i);
+            }
+        } else {
+            if (currentPage <= 3) {
+                pages.push(1, 2, 3, 4, '...');
+            } else if (currentPage >= totalPages - 2) {
+                pages.push(1, '...', totalPages - 3, totalPages - 2, totalPages - 1, totalPages);
+            } else {
+                pages.push(1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPages);
+            }
+        }
+
+        return pages;
+    };
+
 
     return ( 
         <div>
@@ -46,15 +66,20 @@ export default function Table({ Table, TrThead, Th, TrTbody, Td, headers, data, 
             </tbody>
         </table>
          <div className="flex justify-center items-center items-center mt-4 space-x-2">
-                <button onClick={() => changePage(currentData - 1)} className="px-4 py-2 border rounded-md bg-gray-200 hover:bg-gray-300 disabled:bg-gray-100" >
+                <button onClick={() => changePage(currentPage - 1)} className="px-4 py-2 border rounded-md bg-gray-200 hover:bg-gray-300 disabled:bg-gray-100" >
                     Anterior
                 </button>
-                {[...Array(totalPages)].map((_,index) => (
-                    <button key={index} onClick={() => changePage(index + 1)} 
-                    className={`px-4 py-2 border-md ${currentPage === index + 1? 'bg-blue-500 text-white': 'bg-gray-200 hover:bg-gray-300'}`}>
-                         {index + 1}
+              {generatePageList().map((page, index) => (
+                    <button 
+                        key={index} 
+                        onClick={() => page !== '...' && changePage(page)} 
+                        className={`px-4 py-2 border-md ${currentPage === page ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'}`}
+                        disabled={page === '...'}
+                    >
+                        {page}
                     </button>
                 ))}
+
                 <button onClick={() => changePage(currentPage + 1)}
                     className="px-4 py-2 border rounded-md bg-gray-200 hover:bg-gray-300 disabled:gray-100" disabled={currentPage === totalPages}>
                     Próxima
